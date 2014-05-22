69887fcb1bcb72df0a9cfd2dc8a1d877f4856ad6
dut09 <dutao09@gmail.com>
2014-05-22 15:52:53 -0700

    hw4 part1 data

--- src/hw4/hw4_p1.sh
+++ src/hw4/hw4_p1.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+../bin/exrtotiff lines.exr lines.tiff
+../bin/exrtotiff manykilleroos_ref.exr manykilleroos_ref.tiff
+../bin/exrtotiff occluder_ref.exr occluder_ref.tiff
+../bin/exrtotiff occluder_shadow_ref.exr occluder_shadow_ref.tiff
+
+../bin/pbrt occluder_ld_1.pbrt
+../bin/pbrt occluder_ld_2.pbrt
+../bin/pbrt occluder_ld_4.pbrt
+../bin/pbrt occluder_ld_8.pbrt
+../bin/pbrt occluder_ld_16.pbrt
+../bin/pbrt occluder_ld_32.pbrt
+../bin/pbrt occluder_ld_64.pbrt
+../bin/pbrt occluder_rd_1.pbrt
+../bin/pbrt occluder_rd_2.pbrt
+../bin/pbrt occluder_rd_4.pbrt
+../bin/pbrt occluder_rd_8.pbrt
+../bin/pbrt occluder_rd_16.pbrt
+../bin/pbrt occluder_rd_32.pbrt
+../bin/pbrt occluder_rd_64.pbrt
+
 ../bin/exrtotiff occluder_ld_1.exr occluder_ld_1.tiff
 ../bin/exrtotiff occluder_ld_2.exr occluder_ld_2.tiff
 ../bin/exrtotiff occluder_ld_4.exr occluder_ld_4.tiff
@@ -16,17 +36,18 @@
 ../bin/exrtotiff occluder_rd_64.exr occluder_rd_64.tiff
 
 #	difference
-../bin/exrdiff occluder_ld_1.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_2.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_4.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_8.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_16.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_32.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_ld_64.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_1.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_2.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_4.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_8.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_16.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_32.exr occluder_ref.exr >> hw4_p1_diff.txt
-../bin/exrdiff occluder_rd_64.exr occluder_ref.exr >> hw4_p1_diff.txt
+rm hw4_data.txt
+../bin/exrdiff occluder_ld_1.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_2.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_4.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_8.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_16.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_32.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_ld_64.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_1.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_2.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_4.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_8.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_16.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_32.exr occluder_ref.exr >> hw4_data.txt
+../bin/exrdiff occluder_rd_64.exr occluder_ref.exr >> hw4_data.txt
