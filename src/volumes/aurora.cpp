c8accfeac4aa5d68e3f11ee10fba323b52b1b837
dut09 <dutao09@gmail.com>
2014-06-06 00:44:40 -0700

    aurora class
    no generatePhoton yet

--- src/volumes/aurora.cpp
+++ src/volumes/aurora.cpp
@@ -9,6 +9,136 @@ Jun 4, 2014
 #include "volumes/aurora.h"
 #include "paramset.h"
 
+void AuroraGrid::AddPhoton(const AuroraPhoton &photon)
+{
+	Point p = photon.p;
+	if (!extent.Inside(p))
+		return;
+	Vector vox = photon.p - extent.pMin;
+    int vx = int(vox.x / step);
+	int vy = int(vox.y / step);
+	int vz = int(vox.z / step);
+	if (vx == nx || vy == ny || vz == nz)
+		return;
+	grids[vz*nx*ny + vy*nx + vx].photons.push_back(photon);
+}
+
+void AuroraGrid::SearchInGrid(const Point &p, float &r, float &g, float &b) const
+{
+	if (!extent.Inside(p))
+		return;
+	Vector vox = p - extent.pMin;
+	float fx = vox.x / step;
+	float fy = vox.y / step;
+	float fz = vox.z / step;
+	int vx = int(fx); int vy = int(fy); int vz = int(fz);
+	int xmin = fx - vx > 0.5f ? vx : vx - 1;
+	int ymin = fy - vy > 0.5f ? vy : vy - 1;
+	int zmin = fz - vz > 0.5f ? vz : vz - 1;
+	xmin = Clamp(xmin, 0, nx - 1);
+	ymin = Clamp(ymin, 0, ny - 1);
+	zmin = Clamp(zmin, 0, nz - 1);
+	int xmax = xmin + 1; int ymax = ymin + 1; int zmax = zmin + 1;
+	xmax = Clamp(xmax, 0, nx - 1);
+	ymax = Clamp(ymax, 0, ny - 1);
+	zmax = Clamp(zmax, 0, nz - 1);
+
+	//	scan all the grids within the range above
+	r = 0.f;
+	g = 0.f;
+	b = 0.f;
+	float radiusSq = radius * radius;
+	//	sigam in gaussian kernel
+	float sigma = radius;
+	float halfInvSigmaSq = .5f / sigma / sigma;
+	float sum = 0.f;
+	for (int i = xmin; i <= xmax; i++)
+		for (int j = ymin; j <= ymax; j++)
+			for (int k = zmin; k <= zmax; k++)
+			{
+				AuroraVoxel voxel = grids[k*nx*ny + j*nx + i];
+				int photonNum = voxel.PhotonNum();
+				for (int s = 0; s < photonNum; s++)
+				{
+					//	get the position of the photon
+					AuroraPhoton photon = voxel.photons[s];
+					Vector v = photon.p - p;
+					float lenSq = v.LengthSquared();
+					if (lenSq >= radiusSq)
+						continue;
+					//	compute the gaussian weight
+					float weight = expf(-lenSq * halfInvSigmaSq);
+					r += (weight * photon.r);
+					g += (weight * photon.g);
+					b += (weight * photon.b);
+					sum += weight;
+				}
+			}
+	r /= sum;
+	g /= sum;
+	b /= sum;
+}
+
+AuroraDensity::~AuroraDensity()
+{
+	delete []eleDensity;
+}
+
+float AuroraDensity::EleDensity(const Point &Pobj) const
+{
+	if (!extent.Inside(Pobj)) return 0;
+    // Compute voxel coordinates and offsets for _Pobj_
+    Vector vox = extent.Offset(Pobj);
+    vox.x *= nx;
+    vox.y *= ny;
+    vox.z *= nz;
+    int vx = (int)vox.x;
+	int vy = (int)vox.y;
+	int vz = (int)vox.z;
+	if (vx == nx || vy == ny || vz == nz)
+		return 0;
+    float dx = vox.x - vx, dy = vox.y - vy, dz = vox.z - vz;
+    // Trilinearly interpolate density values to compute local density
+    float d00 = Lerp(dx, D(vx, vy, vz),     D(vx+1, vy, vz));
+    float d10 = Lerp(dx, D(vx, vy+1, vz),   D(vx+1, vy+1, vz));
+    float d01 = Lerp(dx, D(vx, vy, vz+1),   D(vx+1, vy, vz+1));
+    float d11 = Lerp(dx, D(vx, vy+1, vz+1), D(vx+1, vy+1, vz+1));
+    float d0 = Lerp(dy, d00, d10);
+    float d1 = Lerp(dy, d01, d11);
+    return Lerp(dz, d0, d1);
+}
+
+float AuroraDensity::Density(const Point &Pobj) const
+{
+    if (!extent.Inside(Pobj)) return 0;
+    float height = Dot(Pobj - extent.pMin, upDir);
+    return a * expf(-b * height);
+}
+
+Spectrum AuroraDensity::Lve(const Point &p, const Vector &, float) const
+{
+	const Point Pobj = WorldToVolume(p);
+	float rgb[3];
+	grid.SearchInGrid(Pobj, rgb[0], rgb[1], rgb[2]);
+	return Spectrum::FromRGB(rgb);
+}
+
+Spectrum AuroraDensity::tau(const Ray &r, float stepSize, float u) const 
+{
+    float t0, t1;
+    float length = r.d.Length();
+    if (length == 0.f) return 0.f;
+    Ray rn(r.o, r.d / length, r.mint * length, r.maxt * length, r.time);
+    if (!IntersectP(rn, &t0, &t1)) return 0.;
+    Spectrum tau(0.);
+    t0 += u * stepSize;
+    while (t0 < t1) {
+        tau += sigma_t(rn(t0), -rn.d, r.time);
+        t0 += stepSize;
+    }
+    return tau * stepSize;
+}
+
 // AuroraDensity Method Definitions
 AuroraDensity *CreateAuroraVolumeRegion(const Transform &volume2world,
         const ParamSet &params) {
@@ -16,10 +146,31 @@ AuroraDensity *CreateAuroraVolumeRegion(const Transform &volume2world,
     Spectrum sigma_a = params.FindOneSpectrum("sigma_a", 0.);
     Spectrum sigma_s = params.FindOneSpectrum("sigma_s", 0.);
     float g = params.FindOneFloat("g", 0.);
-    Spectrum Le = params.FindOneSpectrum("Le", 0.);
     Point p0 = params.FindOnePoint("p0", Point(0,0,0));
     Point p1 = params.FindOnePoint("p1", Point(1,1,1));
+	float a = params.FindOneFloat("a", 1.);
+    float b = params.FindOneFloat("b", 1.);
+    Vector up = params.FindOneVector("updir", Vector(0,1,0));
+	float radius = params.FindOneFloat("radius", 1.);
+	int nitems;
+    const float *data = params.FindFloat("density", &nitems);
+    if (!data) {
+        Error("No \"density\" values provided for volume grid?");
+        return NULL;
+    }
+    int nx = params.FindOneInt("nx", 1);
+    int ny = params.FindOneInt("ny", 1);
+    int nz = params.FindOneInt("nz", 1);
+    if (nitems != nx*ny*nz) {
+        Error("VolumeGridDensity has %d density values but nx*ny*nz = %d",
+            nitems, nx*ny*nz);
+        return NULL;
+    }
+	string rcolor = params.FindOneFilename("aurora_r", "aurora_r.txt.even");
+	string gcolor = params.FindOneFilename("aurora_g", "aurora_g.txt.even");
+	string bcolor = params.FindOneFilename("aurora_b", "aurora_b.txt.even");
+	string intensity = params.FindOneFilename("aurora_intensity", "rays.txt.even");
 	//	TODO: what is our parameter for AuroraDensity?
-	return new AuroraDensity(sigma_a, sigma_s, g, Le, BBox(p0, p1),
-        volume2world);
+	return new AuroraDensity(sigma_a, sigma_s, g, BBox(p0, p1),
+        volume2world, a, b, up, nx, ny, nz, radius, data, rcolor, gcolor, bcolor, intensity);
 }
--- src/volumes/aurora.h
+++ src/volumes/aurora.h
@@ -13,37 +13,186 @@ Jun 4, 2014
 
 // volumes/aurora.h*
 #include "volume.h"
+#include "../core/splines.h"
+
+#include <fstream>
+
+struct AuroraPhoton
+{
+	AuroraPhoton(const Point& pp, float rr, float gg, float bb)
+		: p(pp), r(rr), g(gg), b(bb)
+	{
+	}
+	Point p;
+	float r, g, b;
+};
+
+struct AuroraVoxel
+{
+	AuroraVoxel()
+	{
+		photons.clear();
+	}
+	~AuroraVoxel()
+	{
+		photons.clear();
+	}
+	int PhotonNum() {return (int)photons.size();}
+	vector<AuroraPhoton> photons;
+};
+
+class AuroraGrid
+{
+public:
+	AuroraGrid(const BBox &e, int x, int y, int z, float radius,
+		const string rcolor, const string gcolor, const string bcolor,
+		const string intensity)
+	{
+		Vector vox = e.pMax - e.pMin;
+		float dx = vox.x / x;
+		float dy = vox.y / y;
+		float dz = vox.z / z;
+		step = max(dx, max(dy, dz));
+		step = max(step, 2 * radius);
+		nx = int(vox.x / step) + 1;
+		ny = int(vox.y / step) + 1;
+		nz = int(vox.z / step) + 1;
+		vox.x = step * nx;
+		vox.y = step * ny;
+		vox.z = step * nz;
+		extent.pMin = e.pMin;
+		extent.pMax = e.pMin + vox;
+		grids = new AuroraVoxel[nx * ny * nz];
+
+		//	color and intensity
+		//	read aurora color information
+		std::ifstream fin;
+		float height, value;
+		//	r color
+		fin.open(rcolor);
+		while (!fin.eof())
+		{
+			fin >> height >> value;
+			auroraColor[0].Add_Sample(height, value);
+		}
+		fin.close();
+		//	g color
+		fin.open(gcolor);
+		while (!fin.eof())
+		{
+			fin >> height >> value;
+			auroraColor[1].Add_Sample(height, value);
+		}
+		fin.close();
+		//	b color
+		fin.open(bcolor);
+		while (!fin.eof())
+		{
+			fin >> height >> value;
+			auroraColor[2].Add_Sample(height, value);
+		}
+		fin.close();
+		//	intensity
+		fin.open(intensity);
+		while (!fin.eof())
+		{
+			fin >> height >> value;
+			auroraIntensity.Add_Sample(height, value);
+		}
+		fin.close();
+	}
+	~AuroraGrid(){delete []grids;}
+
+	//	add a new photon into the grid
+	void AddPhoton(const AuroraPhoton &photon);
+	//	given a position p and a search radius, find the nearby photon
+	//	then use gaussian kernel to average them
+	void SearchInGrid(const Point &p, float &r, float &g, float &b) const;
+
+private:
+	BBox extent;
+	float step;
+	float radius;
+	int nx, ny, nz;
+	AuroraVoxel *grids;
+
+	Catmull_Rom auroraColor[3];
+	Catmull_Rom auroraIntensity;
+};
+
 
 // AuroraDensity Declarations
-class AuroraDensity : public DensityRegion {
+class AuroraDensity : public VolumeRegion {
 public:
-    // AuroraDensity Public Methods
-    AuroraDensity(const Spectrum &sa, const Spectrum &ss,
-                       float gg, const Spectrum &emit, const BBox &e,
-                       const Transform &v2w)
-        : DensityRegion(sa, ss, gg, emit, v2w), extent(e) 
+	// AuroraRegion Public Methods
+    AuroraDensity(const Spectrum &sa, const Spectrum &ss, float gg, const BBox &e,
+                  const Transform &VolumeToWorld, float aa, float bb,
+                       const Vector &up, int x, int y, int z, float radius, const float *d,
+					   const string rcolor, const string gcolor, const string bcolor, const string intensity)
+        : sig_a(sa), sig_s(ss), g(gg), extent(e),
+          WorldToVolume(Inverse(VolumeToWorld)), a(aa), b(bb), 
+		  nx(x), ny(y), nz(z), grid(e, nx, ny, nz, radius, rcolor, gcolor, bcolor, intensity)
 	{
+		upDir = Normalize(up);
+		eleDensity = new float[nx*ny*nz];
+        memcpy(eleDensity, d, nx * ny * nz * sizeof(float));
 		//	TODO
-    }
-    BBox WorldBound() const { return Inverse(WorldToVolume)(extent); }
-    bool IntersectP(const Ray &r, float *t0, float *t1) const {
+	}
+	~AuroraDensity();
+	BBox WorldBound() const { return Inverse(WorldToVolume)(extent); }
+	bool IntersectP(const Ray &r, float *t0, float *t1) const 
+	{
         Ray ray = WorldToVolume(r);
         return extent.IntersectP(ray, t0, t1);
     }
-    float Density(const Point &Pobj) const {
-        if (!extent.Inside(Pobj)) return 0;
-		//	TODO: return the density in Pobj
-		/*
-		float height = Dot(Pobj - extent.pMin, upDir);
-        return a * expf(-b * height);
-		*/
-		return 0.f;
+	float Density(const Point &Pobj) const;
+    Spectrum sigma_a(const Point &p, const Vector &, float) const 
+	{
+        return Density(WorldToVolume(p)) * sig_a;
+    }
+    Spectrum sigma_s(const Point &p, const Vector &, float) const 
+	{
+        return Density(WorldToVolume(p)) * sig_s;
+    }
+    Spectrum sigma_t(const Point &p, const Vector &, float) const 
+	{
+        return Density(WorldToVolume(p)) * (sig_a + sig_s);
+    }
+    Spectrum Lve(const Point &p, const Vector &, float) const;	//	TODO
+    float p(const Point &p, const Vector &w, const Vector &wp, float) const 
+	{
+        return PhaseHG(w, wp, g);
+    }
+    Spectrum tau(const Ray &r, float stepSize, float offset) const;
+
+	float EleDensity(const Point &Pobj) const;
+
+	float D(int x, int y, int z) const {
+        x = Clamp(x, 0, nx-1);
+        y = Clamp(y, 0, ny-1);
+        z = Clamp(z, 0, nz-1);
+        return eleDensity[z*nx*ny + y*nx + x];
     }
 private:
-    // AuroraDensity Private Data
-    BBox extent;
-	//	TODO
+	Spectrum sig_a, sig_s;
+    float g;
+    Transform WorldToVolume;
+	BBox extent;
+	
+	//	use a, b and upDir to compute density
+	float a, b;
+    Vector upDir;
+
+	//	eleDensity represents whether aurora 'exists' or not
+	//	use eleDensity to generate the photon in aurora
+	float *eleDensity;
+	//	the number of bins in x, y, z in eDensity
+	int nx, ny, nz;
+
+	//	aurora grid
+	AuroraGrid grid;
 };
+
 
 AuroraDensity *CreateAuroraVolumeRegion(const Transform &volume2world,
         const ParamSet &params);
