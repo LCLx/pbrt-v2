3fc7c652a7f0a1ca83679778bf81fcdc7cfd7deb
Ubuntu <ubuntu@ip-172-31-57-113.ec2.internal>
2017-01-13 01:03:07 +0000

    Update the render script.

--- microstructure/scripts/j12_elip2_3d/render.py
+++ microstructure/scripts/j12_elip2_3d/render.py
@@ -3,15 +3,15 @@
 import os
 import sys
 
-# Arguments: Number of frames Sampling number
-n_frame = int(sys.argv[1])
-scale_factor = float(sys.argv[2])
+n_start = int(sys.argv[1])
+n_end = int(sys.argv[2])
+scale_factor = float(sys.argv[3])
 
 # Now generate pbrt scripts.
 if not os.path.exists('rendering'):
   os.makedirs('rendering')
 
-for i in range(n_frame):
+for i in range(n_start, n_end + 1):
   # Rendering.
   os.system('../../../src/bin/pbrt --ncores 36 --outfile %d/j12_elip2_3d_%03d.exr %d/j12_elip2_3d.pbrt' % (i, i, i))
   os.system('../../../src/bin/exrtotiff -scale %f %d/j12_elip2_3d_%03d.exr rendering/j12_elip2_3d_%03d.tiff' % (scale_factor, i, i, i))
