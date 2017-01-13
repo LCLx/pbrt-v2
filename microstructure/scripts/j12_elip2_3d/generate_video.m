74e7a22cbb6b0c000ab5d2c1da347880098ff088
Tao Du <dutao09@gmail.com>
2017-01-12 21:38:46 -0500

    Generate videos in j12_elip2_3d.

---
+++ microstructure/scripts/j12_elip2_3d/generate_video.m
@@ -0,0 +1,14 @@
+writerObj = VideoWriter('j12_elip2_3d.avi');
+writerObj.FrameRate = 30;
+% open the video writer
+open(writerObj);
+% write the frames to the video
+img_num = length(dir('rendering')) - 2;
+for u = 0 : (img_num - 1)
+ % convert the image to a frame
+ img = imread(['rendering/j12_elip2_3d_', num2str(u, '%03d'), '.tiff']);
+ frame = im2frame(img(:, :, 1 : 3));
+ writeVideo(writerObj, frame);
+end
+% close the writer object
+close(writerObj);
