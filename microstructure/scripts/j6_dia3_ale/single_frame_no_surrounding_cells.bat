9ca8fd335a2925eaa19790d6377de730b9f27035
Tao Du <dutao09@gmail.com>
2017-01-16 15:02:31 -0500

    Update.

--- microstructure/scripts/j6_dia3_ale/single_frame_no_surrounding_cells.bat
+++ microstructure/scripts/j6_dia3_ale/single_frame_no_surrounding_cells.bat
@@ -2,4 +2,4 @@ set frame_index=%1
 set root_folder=\\SCALAR\share_topo\rendering2\j6_dia3_ale\%frame_index%
 ..\..\x64\Release\microstructure.exe mesh.pbrt %root_folder% lattice ale_dis material NULL NULL fine_intf_flags NULL NULL density
 ..\..\..\bin\pbrt.exe --ncores 12 --outfile %1.exr j6_dia3_ale.pbrt
-..\..\..\bin\exrtotiff %1.exr %1.tiff
+..\..\..\bin\exrtotiff %1_no_surrounding_cells.exr %1_no_surrounding_cells.tiff
