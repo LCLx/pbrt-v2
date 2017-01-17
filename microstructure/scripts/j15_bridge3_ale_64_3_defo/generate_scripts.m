8be1c8d5d7899b147f49262bd38d9908dd412601
Tao Du <dutao09@gmail.com>
2017-01-16 19:37:45 -0500

    Update.

--- microstructure/scripts/j15_bridge3_ale_64_3_defo/generate_scripts.m
+++ microstructure/scripts/j15_bridge3_ale_64_3_defo/generate_scripts.m
@@ -1,11 +1,12 @@
 % Tao Du
 % taodu@csail.mit.edu
 % Jan 12, 2017
-root_folder = '\\SCALAR\share_topo\rendering2\j15_bridge3_ale_64_3_defo\';
-n = 78;
+name = 'j15_bridge3_ale_64_3_defo';
+root_folder = ['\\SCALAR\share_topo\rendering2\', name, '\'];
+n = 183;
 
 for i = 0 : n
     mkdir(num2str(i));
-    system(['j15_bridge3_ale_64_3_defo.bat ', num2str(i)]);
-    copyfile('j15_bridge3_ale_64_3_defo.pbrt', num2str(i));
+    system([name, '.bat ', num2str(i)]);
+    copyfile([name, '.pbrt'], num2str(i));
 end
--- microstructure/scripts/j15_canti_ale_2d_4_defo_final/generate_scripts.m
+++ microstructure/scripts/j15_canti_ale_2d_4_defo_final/generate_scripts.m
@@ -5,7 +5,7 @@ name = 'j15_canti_ale_2d_4_defo_final';
 root_folder = ['\\SCALAR\share_topo\rendering2\', name, '\'];
 n = 78;
 
-for i = 0 : n
+for i = 78 : 136
     mkdir(num2str(i));
     system([name, '.bat ', num2str(i)]);
     copyfile([name, '.pbrt'], num2str(i));
