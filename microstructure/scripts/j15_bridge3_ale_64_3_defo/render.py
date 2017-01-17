eb75ca6671f327da81bc3871a8de0200c5af3b21
Tao Du <dutao09@gmail.com>
2017-01-17 02:27:49 +0000

    Update.

--- microstructure/scripts/j15_bridge3_ale_64_3_defo/render.py
+++ microstructure/scripts/j15_bridge3_ale_64_3_defo/render.py
@@ -6,6 +6,9 @@ import sys
 n_start = int(sys.argv[1])
 n_end = int(sys.argv[2])
 scale_factor = float(sys.argv[3])
+n_step = 1
+if len(sys.argv) >= 5:
+  n_step = int(sys.argv[4])
 
 # Now generate pbrt scripts.
 root_folder = '/home/ubuntu/external/j15_bridge3_ale_64_3_defo/rendering'
@@ -13,7 +16,7 @@ root_folder = '/home/ubuntu/external/j15_bridge3_ale_64_3_defo/rendering'
 if not os.path.exists(root_folder):
   os.system('mkdir -p %s' % root_folder)
 
-for i in range(n_start, n_end + 1):
+for i in range(n_start, n_end + 1, n_step):
   # Rendering.
   os.system('../../../src/bin/pbrt --ncores 36 --outfile %s/j15_bridge3_ale_64_3_defo_%03d.exr %d/j15_bridge3_ale_64_3_defo.pbrt' % (root_folder, i, i))
   os.system('../../../src/bin/exrtotiff -scale %f %s/j15_bridge3_ale_64_3_defo_%03d.exr %s/j15_bridge3_ale_64_3_defo_%03d.tiff' % (scale_factor, root_folder, i, root_folder, i))
--- microstructure/scripts/j15_canti_ale_2d_4_defo_final/render.py
+++ microstructure/scripts/j15_canti_ale_2d_4_defo_final/render.py
@@ -6,6 +6,10 @@ import sys
 n_start = int(sys.argv[1])
 n_end = int(sys.argv[2])
 scale_factor = float(sys.argv[3])
+n_step = 1
+if len(sys.argv) >= 5:
+  n_step = int(sys.argv[4])
+print n_step
 
 # Now generate pbrt scripts.
 name = 'j15_canti_ale_2d_4_defo_final'
@@ -14,7 +18,8 @@ root_folder = '/home/ubuntu/external/' + name + '/rendering'
 if not os.path.exists(root_folder):
   os.system('mkdir -p %s' % root_folder)
 
-for i in range(n_start, n_end + 1):
+for i in range(n_start, n_end + 1, n_step):
   # Rendering.
   os.system('../../../src/bin/pbrt --ncores 36 --outfile %s/%s_%03d.exr %d/%s.pbrt' % (root_folder, name, i, i, name))
   os.system('../../../src/bin/exrtotiff -scale %f %s/%s_%03d.exr %s/%s_%03d.tiff' % (scale_factor, root_folder, name, i, root_folder, name, i))
+
