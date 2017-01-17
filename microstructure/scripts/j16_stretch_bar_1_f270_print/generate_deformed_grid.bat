9edaa385e03870005855995690536630400874fa
Tao Du <dutao09@gmail.com>
2017-01-17 13:28:39 -0500

    Update.

---
+++ microstructure/scripts/j16_stretch_bar_1_f270_print/generate_deformed_grid.bat
@@ -0,0 +1,2 @@
+set root_folder=\\SCALAR\share_topo\rendering2\j16_stretch_bar_1_f270_print\0\
+..\..\x64\Release\microstructure.exe grid.pbrt %root_folder% coarse_grid coarse_ale_dis coarse_material NULL NULL NULL NULL NULL NULL 1 0 0 v_0 v_1 v_2 v_3 fem_dis_0
