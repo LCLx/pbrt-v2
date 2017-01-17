d313a395f7b8269f21688385b017fe1f71020fcc
Tao Du <dutao09@gmail.com>
2017-01-17 01:14:05 -0500

    Update.

--- microstructure/scripts/j15_bridge3_ale_64_3_final/single_frame_no_surrounding_cells.bat
+++ microstructure/scripts/j15_bridge3_ale_64_3_final/single_frame_no_surrounding_cells.bat
@@ -1,6 +1,7 @@
 set frame_index=%1
+set threshold=%2
 set name=j15_bridge3_ale_64_3_final
 set root_folder=\\SCALAR\share_topo\rendering2\%name%\%frame_index%
-..\..\x64\Release\microstructure.exe mesh.pbrt %root_folder% lattice ale_dis material NULL NULL fine_intf_flags NULL NULL density 0
+..\..\x64\Release\microstructure.exe mesh.pbrt %root_folder% lattice ale_dis material NULL NULL fine_intf_flags NULL NULL density 0 %threshold%
 ..\..\..\bin\pbrt.exe --ncores 12 --outfile %1_no_surrounding_cells.exr %name%.pbrt
 ..\..\..\bin\exrtotiff %1_no_surrounding_cells.exr %1_no_surrounding_cells.tiff
