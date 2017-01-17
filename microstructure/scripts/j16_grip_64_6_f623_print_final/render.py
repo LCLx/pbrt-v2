9653ca7ad7691c66390be833fdba7a4a00d9a1a9
Tao Du <dutao09@gmail.com>
2017-01-17 02:59:55 +0000

    Update.

--- microstructure/scripts/j16_grip_64_6_f623_print_final/render.py
+++ microstructure/scripts/j16_grip_64_6_f623_print_final/render.py
@@ -18,8 +18,8 @@ root_folder = '/home/ubuntu/external/' + name + '/rendering'
 if not os.path.exists(root_folder):
   os.system('mkdir -p %s' % root_folder)
 
-for i in range(14, n_frame):
-  f = open(name + '.pbrt', 'w')
+for i in range(0, n_frame):
+  f = open(name + '_camera.pbrt', 'w')
   f.write('Film "image" "integer xresolution" [1600] "integer yresolution" [1200]\n')
   f.write('LookAt 3.5 4 -10 0.0 0.0 0.0 0.0 1.0 0.0\n')
   f.write('Camera "perspective" "float fov" [15]\n')
@@ -49,7 +49,7 @@ for i in range(14, n_frame):
   f.write('AttributeBegin\n')
   f.write('Material "matte" "rgb Kd" [0.28 0.3 0.34]\n')
   f.write('TransformBegin\n')
-  f.write('  Translate 0 -0.3 0\n')
+  f.write('  Translate 0 -0.2 0\n')
   f.write('  Scale 100 1 100\n')
   f.write('  Shape "trianglemesh" "integer indices" [0 1 2 2 1 3]\n')
   f.write('  "point P" [-1 -1 -1 -1 -1 1 1 -1 -1 1 -1 1]\n')
@@ -58,12 +58,12 @@ for i in range(14, n_frame):
   f.write('TransformBegin\n')
   f.write('  Rotate %f 0 1 0\n' % (i * delta))
   f.write('  Scale 2.7 2.7 2.7\n')
-  f.write('  Translate -0.5 -0.4 0\n')
+  f.write('  Translate -0.5 -0.3 0\n')
   f.write('  Include "mesh.pbrt"\n')
   f.write('TransformEnd\n')
   f.write('WorldEnd\n')
   f.close()
 
   # Rendering.
-  os.system('../../../src/bin/pbrt --ncores 36 --outfile %s/%s_%03d.exr %s.pbrt' % (root_folder, name, i, name))
+  os.system('../../../src/bin/pbrt --ncores 36 --outfile %s/%s_%03d.exr %s_camera.pbrt' % (root_folder, name, i, name))
   os.system('../../../src/bin/exrtotiff -scale %f %s/%s_%03d.exr %s/%s_%03d.tiff' % (scale_factor, root_folder, name, i, root_folder, name, i))
