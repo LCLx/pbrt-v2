3258836f780c91ecc5b50c555c306e0c6b75c029
Tao Du <dutao09@gmail.com>
2017-01-17 13:16:18 -0500

    Update.

--- microstructure/scripts/j12_elip2_3d/j12_elip2_3d_with_fine_intf_flag.bat
+++ microstructure/scripts/j12_elip2_3d/j12_elip2_3d_with_fine_intf_flag.bat
@@ -1,5 +1,5 @@
 set frame_index=%1
 set root_folder=\\SCALAR\share_topo\rendering2\j12_elip2_3d\%frame_index%
-..\..\x64\Release\microstructure.exe j12_elip2_3d_mesh.pbrt %root_folder% lattice ale_dis material NULL NULL fine_intf_flags
-..\..\..\bin\pbrt.exe --ncores 12 --outfile j12_elip2_3d_000_with_fine_intf_flags.exr j12_elip2_3d.pbrt
-..\..\..\bin\exrtotiff -scale 2 j12_elip2_3d_000_with_fine_intf_flags.exr j12_elip2_3d_000_with_fine_intf_flags.tiff
+..\..\x64\Release\microstructure.exe j12_elip2_3d_mesh.pbrt %root_folder% lattice ale_dis material NULL sing_points fine_intf_flags
+..\..\..\bin\pbrt.exe --ncores 12 --outfile j12_elip2_3d_00%frame_index%_with_fine_intf_flags.exr j12_elip2_3d.pbrt
+..\..\..\bin\exrtotiff -scale 2 j12_elip2_3d_00%frame_index%_with_fine_intf_flags.exr j12_elip2_3d_00%frame_index%_with_fine_intf_flags.tiff
--- microstructure/scripts/j14_sin_ani_2/j14_sin_ani_2_with_fine_intf_flag.bat
+++ microstructure/scripts/j14_sin_ani_2/j14_sin_ani_2_with_fine_intf_flag.bat
@@ -1,5 +1,5 @@
 set frame_index=%1
 set root_folder=\\SCALAR\share_topo\rendering2\j14_sin_ani_2\%frame_index%
-..\..\x64\Release\microstructure.exe j14_sin_ani_2_mesh.pbrt %root_folder% lattice ale_dis material NULL NULL fine_intf_flags
+..\..\x64\Release\microstructure.exe j14_sin_ani_2_mesh.pbrt %root_folder% lattice ale_dis material NULL sing_points fine_intf_flags
 ..\..\..\bin\pbrt.exe --ncores 12 --outfile j14_sin_ani_2_00%frame_index%_with_fine_intf_flags.exr j14_sin_ani_2.pbrt
 ..\..\..\bin\exrtotiff -scale 2 j14_sin_ani_2_00%frame_index%_with_fine_intf_flags.exr j14_sin_ani_2_00%frame_index%_with_fine_intf_flags.tiff
